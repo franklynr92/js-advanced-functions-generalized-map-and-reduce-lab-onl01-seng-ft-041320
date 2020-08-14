224f3e24def3d4707f4e9658b0764f4c95861e6e
Franklyn Rodriguez <franklynr92@gmail.com>
2020-08-14 19:13:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your functions here
+function map(arr, someFunc){
+    let newArr= [];
+    for(let i = 0; i < arr.length; i++){
+        arr[i]; 
+        newArr.push(someFunc(arr[i]));
+    }
+    return newArr
+}
+
+
+function reduce(arr, somefunc, startingPoint=0){
+    for(let i = 0; i < arr.length; i++){
+        somefunc(arr[i], startingPoint)}
+        return somefunc();
+}
+// function reduceToAllTrue(sourceArray){
+//     for (let i = 0; i < sourceArray.length; i++){
+//         if (!sourceArray[i]){ 
+//             return false
+//         }
+//     }
+//     return true
+// }
+
+// function reduceToAnyTrue(sourceArray){
+//     for (let i = 0; i < sourceArray.length; i++){
+//         if (!!sourceArray[i]){ 
+//             return true
+//         }
+//     }
+//     return false
+// }
